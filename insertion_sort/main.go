608cffac49a7dfba95d12eb9f5adbeaaa2b3f308
pankratsdarya <dapankrats@mail.ru>
2021-02-18 22:19:37 +0700

    Вставки и пузырёк
    Сортировка вставками оформлена как функция.
    Добавлена сортировка пузырьком.

--- insertion_sort/main.go
+++ insertion_sort/main.go
@@ -2,14 +2,42 @@ package main
 
 import "fmt"
 
-func main() {
-
-	arr := [...]int{0, 4, 7, 2, 6, 1, 3, 5, 9, 8}
-
-	for i := 2; i < cap(arr); i++ {
-		for j := i; j > 0 && arr[j] < arr[j-1]; j-- {
-			arr[j], arr[j-1] = arr[j-1], arr[j]
+// bubble sort
+func bubble(src []int) []int {
+	dest := make([]int, len(src))
+	copy(dest, src)
+	for j := 1; j < len(src)-1; j++ {
+		for i := 0; i < len(src)-j; i++ {
+			if dest[i] > dest[i+1] {
+				dest[i], dest[i+1] = dest[i+1], dest[i]
+			}
 		}
 	}
-	fmt.Println(arr)
+	return dest
+}
+
+// insertions sort
+func insertion(src []int) []int {
+	dest := make([]int, len(src))
+	copy(dest, src)
+	for i := 2; i < len(dest); i++ {
+		for j := i; j > 0 && dest[j] < dest[j-1]; j-- {
+			dest[j], dest[j-1] = dest[j-1], dest[j]
+		}
+	}
+	return dest
+}
+
+func main() {
+
+	arrOne := []int{7, 2, 0, 4, 9, 8, 6, 1, 3, 5}
+	arrTwo := []int{0, 4, 7, 2, 6, 1, 3, 5, 9, 8}
+
+	arrOneSort := bubble(arrOne)
+	arrTwoSort := insertion(arrTwo)
+
+	fmt.Println(arrOne)
+	fmt.Println(arrOneSort)
+	fmt.Println(arrTwo)
+	fmt.Println(arrTwoSort)
 }
